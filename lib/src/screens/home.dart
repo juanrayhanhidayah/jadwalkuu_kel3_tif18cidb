a4e8b3b53c6a5928d71c8161f9521ce7b9b015bf
juanrayhanhidayah <73089275+juanrayhanhidayah@users.noreply.github.com>
2021-01-05 16:37:07 +0700

    pembuatan date picker error pas pindah route

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,22 +1,71 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:jadwal_app/src/screens/add_item.dart';
 import 'package:jadwal_app/src/screens/login.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
-  final auth = FirebaseAuth.instance;
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  CalendarController _calendarController = CalendarController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FlatButton(
-          child: Text('Logout'),
-          onPressed: () {
-            auth.signOut();
-            Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => LoginScreen(),
-            ));
-          },
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.person), onPressed: () {})
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Card(
+              clipBehavior: Clip.antiAlias,
+              margin: const EdgeInsets.all(8.0),
+              child: TableCalendar(
+                calendarController: _calendarController,
+                weekendDays: [7],
+                headerStyle: HeaderStyle(
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                  ),
+                  headerMargin: const EdgeInsets.only(bottom: 8.0),
+                  titleTextStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  formatButtonDecoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  formatButtonTextStyle: TextStyle(color: Colors.white),
+                  leftChevronIcon: Icon(
+                    Icons.chevron_left,
+                    color: Colors.white,
+                  ),
+                  rightChevronIcon: Icon(
+                    Icons.chevron_right,
+                    color: Colors.white,
+                  ),
+                ),
+                calendarStyle: CalendarStyle(),
+                builders: CalendarBuilders(),
+              ),
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => AddItem()));
+        },
       ),
     );
   }
