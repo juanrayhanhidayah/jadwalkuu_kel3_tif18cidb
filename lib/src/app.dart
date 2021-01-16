c1efa7ddc3f55d902fa37ece67b1bced5b226427
juanrayhanhidayah <73089275+juanrayhanhidayah@users.noreply.github.com>
2021-01-16 09:26:23 +0700

    CRUD beres tinggal delete nya aja

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:jadwal_app/src/providers/entry_providers.dart';
 import 'package:jadwal_app/src/screens/login.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Jadwalku',
-      theme:
-          ThemeData(accentColor: Colors.blueAccent, primarySwatch: Colors.blue),
-      home: LoginScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => EntryProvider(),
+      child: MaterialApp(
+          home: LoginScreen(),
+          theme: ThemeData(
+            accentColor: Colors.blueAccent,
+            primaryColor: Colors.blue,
+          )),
     );
   }
 }
--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,69 +1,50 @@
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:jadwal_app/src/screens/add_item.dart';
+import 'package:jadwal_app/src/model/entry.dart';
+import 'package:jadwal_app/src/providers/entry_providers.dart';
+import 'package:jadwal_app/src/screens/entry.dart';
+import 'package:provider/provider.dart';
 
-import 'package:table_calendar/table_calendar.dart';
-
-class HomeScreen extends StatefulWidget {
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  CalendarController _calendarController = CalendarController();
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final entryProvider = Provider.of<EntryProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dashboard'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.person), onPressed: () {})
-        ],
+        title: Text('Jadwalku'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Card(
-              clipBehavior: Clip.antiAlias,
-              margin: const EdgeInsets.all(8.0),
-              child: TableCalendar(
-                calendarController: _calendarController,
-                weekendDays: [7],
-                headerStyle: HeaderStyle(
-                  decoration: BoxDecoration(
-                    color: Colors.blueAccent,
-                  ),
-                  headerMargin: const EdgeInsets.only(bottom: 8.0),
-                  titleTextStyle: TextStyle(
-                    color: Colors.white,
-                  ),
-                  formatButtonDecoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  formatButtonTextStyle: TextStyle(color: Colors.white),
-                  leftChevronIcon: Icon(
-                    Icons.chevron_left,
-                    color: Colors.white,
-                  ),
-                  rightChevronIcon: Icon(
-                    Icons.chevron_right,
-                    color: Colors.white,
-                  ),
-                ),
-                calendarStyle: CalendarStyle(),
-                builders: CalendarBuilders(),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: StreamBuilder<List<Entry>>(
+          stream: entryProvider.entries,
+          builder: (context, snapshot) {
+            return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 7, horizontal: 7),
+                    elevation: 4,
+                    child: ListTile(
+                      hoverColor: Colors.blueAccent,
+                      trailing: Icon(Icons.edit,
+                          color: Theme.of(context).accentColor),
+                      title: Text(snapshot.data[index].entry),
+                      subtitle: Text(
+                        formatDate(DateTime.parse(snapshot.data[index].date),
+                            [MM, ' ', d, ', ', yyyy]),
+                      ),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                EntryScreen(entry: snapshot.data[index])));
+                      },
+                    ),
+                  );
+                });
+          }),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => AddItem()));
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => EntryScreen()));
         },
       ),
     );
